82844aa093adb2f8a466e5686530abf9e28e917a
山本健剛 <yamamotokengo@yamamotokengounoMacBook-Air.local>
2019-09-30 15:53:59 +0900

    gem ranking

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -1,13 +1,15 @@
 class RanksController < ApplicationController
   def index
-    if  params(:query).blank?
+    if  params[:query].blank?
         @items = Item.ranking_by_average
-    elsif params(:query) == "week"
-        @items = Item.ranking_by_week
-    elsif params(:query) == "month"
-        @items = Item.ranking_by_month
-    elsif params(:quety) == "year"
-        @items = Item.ranking_by_year
+    elsif params[:query] == "week"
+        @items = Item.ranking_by_duration_week
+    elsif params[:query] == "month"
+        @items = Item.ranking_by_duration_month
+    elsif params[:query] == "year"
+        @items = Item.ranking_by_duration_year
+        p @items
     end
+  
   end
 end
